f96e50ceec4deb4011257babb47135db93d6770e
George Kaplan <george@georgekaplan.xyz>
2020-06-05 10:42:34 -0400

    move lsp config to dedicated lua file

--- .config/nvim/init.vim
+++ .config/nvim/init.vim
@@ -5,16 +5,4 @@ packadd nvim-lsp
 au VimEnter * highlight Pmenu ctermfg=7 ctermbg=0
 au VimEnter * highlight SpellBad ctermbg=1
 
-lua <<EOF
-  local lsp = require'nvim_lsp'
-
-  lsp.bashls.setup{}
-  lsp.cssls.setup{}
-  lsp.dockerls.setup{}
-  lsp.gopls.setup{}
-  lsp.html.setup{}
-  lsp.jsonls.setup{}
-  lsp.rust_analyzer.setup{}
-  lsp.tsserver.setup{}
-  lsp.vimls.setup{}
-EOF
+lua require'lsp'
---
+++ .config/nvim/lua/lsp.lua
@@ -0,0 +1,29 @@
+do
+  local method = "textDocument/publishDiagnostics"
+  local default_callback = vim.lsp.callbacks[method]
+  vim.lsp.callbacks[method] = function(err, method, result, client_id)
+    default_callback(err, method, result, client_id)
+    if result and result.diagnostics then
+      for _, v in ipairs(result.diagnostics) do
+        v.bufnr = client_id
+        v.lnum = v.range.start.line + 1
+        v.col = v.range.start.character + 1
+        v.text = v.message
+      end
+      vim.lsp.util.set_loclist(result.diagnostics)
+    end
+  end
+end
+
+
+local lsp = require'nvim_lsp'
+
+lsp.bashls.setup{}
+lsp.cssls.setup{}
+lsp.dockerls.setup{}
+lsp.gopls.setup{}
+lsp.html.setup{}
+lsp.jsonls.setup{}
+lsp.rust_analyzer.setup{}
+lsp.tsserver.setup{}
+lsp.vimls.setup{}
