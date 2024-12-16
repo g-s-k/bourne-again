44cf1446f8e551fd8863ee02148f3cca4e4eaf44
George Kaplan <george@georgekaplan.xyz>
2024-12-16 12:17:33 +0100

    add git-awareness to fzf binding callback

--- .config/nvim/lua/tools/fzf.lua
+++ .config/nvim/lua/tools/fzf.lua
@@ -18,7 +18,19 @@ if vim.env.TMUX then
   vim.g.fzf_layout = { tmux = '90%,60%' }
 end
 
-vim.keymap.set('n', '<leader>f', ':FZF<CR>', { silent = true })
+vim.keymap.set('n', '<leader>f', function()
+  if vim.fs.root(0, { '.git' }) then
+    vim.fn['fzf#run'](
+      vim.fn['fzf#wrap']({
+        source = 'git ls-files',
+        sink = 'e',
+      })
+    )
+  else
+    vim.cmd('FZF')
+  end
+end, { silent = true })
+
 vim.keymap.set('n', '<leader>b', function()
   local source = {}
   for k, v in pairs(vim.fn.getbufinfo({ buflisted = 1 })) do
