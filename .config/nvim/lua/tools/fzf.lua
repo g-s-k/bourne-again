f7d7ef57381c8ff706b421f8ae2ea0a730591cd6
George Kaplan <george@georgekaplan.xyz>
2024-12-16 01:33:57 +0100

    auto-format Lua

--- .config/nvim/lua/tools/fzf.lua
+++ .config/nvim/lua/tools/fzf.lua
@@ -1,35 +1,35 @@
 if vim.fn.isdirectory('/opt/homebrew/opt/fzf') ~= 0 then
-	vim.opt.rtp:append { '/opt/homebrew/opt/fzf' }
+  vim.opt.rtp:append { '/opt/homebrew/opt/fzf' }
 end
 
 local nix_fzf_path = string.gsub(
-	vim.fn.system {
-		'sh', '-c', 'realpath "$(which fzf)/../../share/vim-plugins/fzf"'
-	},
-	'(.-)%s*$',
-	'%1'
+  vim.fn.system {
+    'sh', '-c', 'realpath "$(which fzf)/../../share/vim-plugins/fzf"'
+  },
+  '(.-)%s*$',
+  '%1'
 )
 
 if vim.fn.isdirectory(nix_fzf_path) ~= 0 then
-	vim.opt.rtp:append { nix_fzf_path }
+  vim.opt.rtp:append { nix_fzf_path }
 end
 
 if vim.env.TMUX then
-	vim.g.fzf_layout = { tmux = '90%,60%' }
+  vim.g.fzf_layout = { tmux = '90%,60%' }
 end
 
 vim.keymap.set('n', '<leader>f', ':FZF<CR>', { silent = true })
 vim.keymap.set('n', '<leader>b', function()
-	local source = {}
-	for k,v in pairs(vim.fn.getbufinfo({buflisted = 1})) do
-		if v.name ~= '' then
-			table.insert(source, v.name)
-		end
-	end
-	vim.fn['fzf#run'](
-		vim.fn['fzf#wrap']({
-			source = source,
-			sink = 'b',
-		})
-	)
+  local source = {}
+  for k, v in pairs(vim.fn.getbufinfo({ buflisted = 1 })) do
+    if v.name ~= '' then
+      table.insert(source, v.name)
+    end
+  end
+  vim.fn['fzf#run'](
+    vim.fn['fzf#wrap']({
+      source = source,
+      sink = 'b',
+    })
+  )
 end, { silent = true })
--- .config/nvim/lua/tools/lsp.lua
+++ .config/nvim/lua/tools/lsp.lua
@@ -3,42 +3,42 @@ vim.keymap.set('n', '<leader>d', vim.diagnostic.setloclist, { silent = true })
 vim.keymap.set('n', '<leader>D', vim.diagnostic.setqflist, { silent = true })
 
 vim.api.nvim_create_autocmd('LspAttach', {
-	callback = function(args)
-		local client = vim.lsp.get_client_by_id(args.data.client_id)
-		if client.supports_method('textDocument/codeAction') then
-			vim.keymap.set({'n', 'v'}, '<leader>a', vim.lsp.buf.code_action, { silent = true })
-		end
-		if client.supports_method('textDocument/definition') then
-			vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { silent = true })
-		end
-		if client.supports_method('textDocument/formatting') then
-			vim.keymap.set({'n', 'v'}, '<leader>F', vim.lsp.buf.format, { silent = true })
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    if client.supports_method('textDocument/codeAction') then
+      vim.keymap.set({ 'n', 'v' }, '<leader>a', vim.lsp.buf.code_action, { silent = true })
+    end
+    if client.supports_method('textDocument/definition') then
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { silent = true })
+    end
+    if client.supports_method('textDocument/formatting') then
+      vim.keymap.set({ 'n', 'v' }, '<leader>F', vim.lsp.buf.format, { silent = true })
 
-			vim.api.nvim_create_autocmd('BufWritePre', {
-				callback = function(args)
-					vim.lsp.buf.format({ timeout_ms = 500 })
-				end,
-			})
-		end
-		if client.supports_method('textDocument/implementation*') then
-			vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, { silent = true })
-		end
-		if client.supports_method('textDocument/references') then
-			vim.keymap.set('n', 'gr', vim.lsp.buf.references, { silent = true })
-		end
-		if client.supports_method('textDocument/rename') then
-			vim.keymap.set('n', '<leader>r', vim.lsp.buf.rename, { silent = true })
-		end
-	end,
+      vim.api.nvim_create_autocmd('BufWritePre', {
+        callback = function(args)
+          vim.lsp.buf.format({ timeout_ms = 500 })
+        end,
+      })
+    end
+    if client.supports_method('textDocument/implementation*') then
+      vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, { silent = true })
+    end
+    if client.supports_method('textDocument/references') then
+      vim.keymap.set('n', 'gr', vim.lsp.buf.references, { silent = true })
+    end
+    if client.supports_method('textDocument/rename') then
+      vim.keymap.set('n', '<leader>r', vim.lsp.buf.rename, { silent = true })
+    end
+  end,
 })
 
 vim.api.nvim_create_autocmd('FileType', {
-  pattern = {'c', 'cpp'},
+  pattern = { 'c', 'cpp' },
   callback = function(ev)
     vim.lsp.start({
       name = 'clangd',
-      cmd = {'clangd', '--clang-tidy'},
-      root_dir = vim.fs.root(ev.buf, {'CMakeLists.txt', 'Makefile', 'README.md'}),
+      cmd = { 'clangd', '--clang-tidy' },
+      root_dir = vim.fs.root(ev.buf, { 'CMakeLists.txt', 'Makefile', 'README.md' }),
     })
   end
 })
@@ -48,8 +48,8 @@ vim.api.nvim_create_autocmd('FileType', {
   callback = function(ev)
     vim.lsp.start({
       name = 'rust-analyzer',
-      cmd = {'rust-analyzer'},
-      root_dir = vim.fs.root(ev.buf, {'Cargo.lock'}),
+      cmd = { 'rust-analyzer' },
+      root_dir = vim.fs.root(ev.buf, { 'Cargo.lock' }),
     })
   end
 })
@@ -59,8 +59,8 @@ vim.api.nvim_create_autocmd('FileType', {
   callback = function(ev)
     vim.lsp.start({
       name = 'zls',
-      cmd = {'zls'},
-      root_dir = vim.fs.root(ev.buf, {'build.zig'}),
+      cmd = { 'zls' },
+      root_dir = vim.fs.root(ev.buf, { 'build.zig' }),
     })
   end
 })
