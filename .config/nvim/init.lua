c86ea7d336174ce2e1e43fb30d4a0e54abf85ad9
George Kaplan <george@georgekaplan.xyz>
2024-12-17 02:49:40 +0100

    add a command to run top/htop

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -6,3 +6,9 @@ require('tools.git')
 require("tools.grep")
 require("tools.lsp")
 require("tools.tmux")
+
+vim.api.nvim_create_user_command('Top', function()
+  require("util.float").run_shell {
+    cmd = vim.fn.executable('htop') and 'htop' or 'top',
+  }
+end, {})
