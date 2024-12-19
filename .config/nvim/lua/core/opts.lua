ccf0f06a84654706213a8837b756e2b40926b423
George Kaplan <george@georgekaplan.xyz>
2024-12-19 02:41:16 +0100

    give up on treesitter folding in rust

--- .config/nvim/lua/core/opts.lua
+++ .config/nvim/lua/core/opts.lua
@@ -40,3 +40,10 @@ vim.opt.foldnestmax = 4
 vim.opt.foldtext = ""
 vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'rust',
+  callback = function(ev)
+    vim.opt_local.foldmethod = 'syntax'
+  end
+})
