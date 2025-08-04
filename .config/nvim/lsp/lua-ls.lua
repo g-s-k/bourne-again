6716d519d5fdab1ba8a71c3aaa2c01c20c068c64
George Kaplan <george@georgekaplan.xyz>
2025-08-04 22:37:37 +0200

    fix annoying lua warnings

--- .config/nvim/lsp/lua-ls.lua
+++ .config/nvim/lsp/lua-ls.lua
@@ -1,4 +1,11 @@
 return {
   cmd = { 'lua-language-server' },
   filetypes = { 'lua' },
+  settings = {
+    Lua = {
+      workspace = {
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+    },
+  },
 };
